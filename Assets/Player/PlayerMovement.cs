c8c671f6061782cd5221dddc3b55f5874c4839de
kringel118 <25124427+nevsky118@users.noreply.github.com>
2020-11-19 01:31:07 +0300

    delegates

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -2,30 +2,61 @@ using System;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
+
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+    bool isInDirectMode = false;
+
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
-
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
 
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-        m_Character = GetComponent<ThirdPersonCharacter>();
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
         currentClickTarget = transform.position;
     }
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0) )
+        if (Input.GetKeyDown(KeyCode.G)) // G for gamepad
         {
-            print("Cursor raycast hit layer: " + cameraRaycaster.layerHit);
-            switch (cameraRaycaster.layerHit)
+            isInDirectMode = !isInDirectMode; // toggle mode
+            currentClickTarget = transform.position; // clear the clicktarget
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement(); // mouse movement
+        }
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = v * cameraForward + h * Camera.main.transform.right;
+        thirdPersonCharacter.Move(movement, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit layer: " + cameraRaycaster.currentLayerHit);
+            switch (cameraRaycaster.currentLayerHit)
             {
                 case Layer.Walkable:
                     currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
@@ -38,14 +69,15 @@ public class PlayerMovement : MonoBehaviour
                     return;
             }
         }
+
         var playerToClickPoint = currentClickTarget - transform.position;
         if (playerToClickPoint.magnitude >= walkMoveStopRadius)
         {
-            m_Character.Move(playerToClickPoint, false, false);
+            thirdPersonCharacter.Move(playerToClickPoint, false, false);
         }
         else
         {
-            m_Character.Move(Vector3.zero, false, false);
+            thirdPersonCharacter.Move(Vector3.zero, false, false);
         }
     }
 }
