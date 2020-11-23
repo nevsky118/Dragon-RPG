9cc46060df41a2142a21be47dd75560e7d848809
kringel118 <25124427+nevsky118@users.noreply.github.com>
2020-11-24 00:41:23 +0300

    navmesh

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.Characters.ThirdPerson;
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] float attackRadius = 4f;
     [SerializeField] float maxHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
+    GameObject player = null;
 
     float currentHealthPoints = 100f;
 
@@ -15,4 +18,24 @@ public class Enemy : MonoBehaviour {
             return currentHealthPoints / maxHealthPoints;
         }
     }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
+    }
+
 }
