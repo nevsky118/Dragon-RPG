69278e63fe3c3d08e0758644abed721210c95738
Vadim <nevsky118@gmail.com>
2020-12-08 18:01:55 +0300

    animatorOverrideContoller

---
+++ Assets/_Characters/Player/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
+using RPG.CameraUI;
+
+
+
+namespace RPG.Characters
+{
+    [RequireComponent(typeof(AICharacterControl))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        ThirdPersonCharacter thirdPersonCharacter = null;   // A reference to the ThirdPersonCharacter on the object
+        CameraRaycaster cameraRaycaster = null;
+        Vector3 clickPoint;
+        AICharacterControl aiCharacterControl = null;
+        GameObject walkTarget = null;
+
+        [SerializeField] const int walkableLayerNumber = 8;
+        [SerializeField] const int enemyLayerNumber = 9;
+
+        bool isInDirectMode = false;
+
+        void Start()
+        {
+            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+            aiCharacterControl = GetComponent<AICharacterControl>();
+            walkTarget = new GameObject("walkTarget");
+
+            cameraRaycaster.notifyMouseClickObservers += ProcessMouseClick;
+        }
+
+        void ProcessMouseClick(RaycastHit raycastHit, int layerHit)
+        {
+            switch (layerHit)
+            {
+                case enemyLayerNumber:
+                    GameObject enemy = raycastHit.collider.gameObject;
+                    aiCharacterControl.SetTarget(enemy.transform);
+                    break;
+                case walkableLayerNumber:
+                    walkTarget.transform.position = raycastHit.point;
+                    aiCharacterControl.SetTarget(walkTarget.transform);
+                    break;
+                default:
+                    Debug.LogWarning("Don't know how to handle mouse click for player movenemt");
+                    return;
+            }
+
+        }
+
+        void ProcessDirectMovement()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            // calculate camera relative direction to move:
+            Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+            Vector3 movement = v * cameraForward + h * Camera.main.transform.right;
+
+            thirdPersonCharacter.Move(movement, false, false);
+        }
+
+
+    }
+
+
+}
---
+++ Assets/_Weapons/Projectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+namespace RPG.Weapons
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] float projectileSpeed;
+
+        [SerializeField] GameObject shooter;
+        float damageCaused;
+
+        public void SetShooter(GameObject shooter)
+        {
+            this.shooter = shooter;
+        }
+
+        public void SetDamage(float damage)
+        {
+            damageCaused = damage;
+        }
+
+        public float GetDefaultLaunchSpeed()
+        {
+            return projectileSpeed;
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+
+            var layerCollidedWith = collision.gameObject.layer;
+            if (shooter && layerCollidedWith != shooter.layer)
+            {
+                DamageDamageables(collision);
+            }
+        }
+
+        private void DamageDamageables(Collision collision)
+        {
+            Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+            if (damageableComponent)
+            {
+                (damageableComponent as IDamageable).TakeDamage(damageCaused);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+}
